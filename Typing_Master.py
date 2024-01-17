4d21f0e2538c581cad4688bb4b35f5fda3e77c9b
kumarroshan123 <kumar30roshan@gmail.com>
2024-01-17 16:30:59 +0530

    Input_Taking

---
+++ Typing_Master.py
@@ -0,0 +1,9 @@
+def main():
+    print("Welcome to Terminal Typing Master!")
+    username = input("Enter your username: ")
+
+    while True:
+        print("\nOptions:")
+        print("1. Start Typing Test")
+        print("2. Show Leaderboard")
+        print("3. Exit")
